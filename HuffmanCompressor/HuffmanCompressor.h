a035bb38df59129f983b9485268c3f7339071092
Apfeltasche20 <turm2004@web.de>
2026-06-11 20:28:16 +0200

    Fixed Huffman Header

--- HuffmanCompressor/HuffmanCompressor.h
+++ HuffmanCompressor/HuffmanCompressor.h
@@ -2,4 +2,5 @@
 #include "HuffmanTree.h"
 #include <stdint.h>
 
-uint8_t* huffman_encode_data(uint8_t* input_data, uint64_t input_data_length, huffman_tree_node* root, uint64_t* output_data_size);
+uint8_t* huffman_encode_data(uint8_t* input_data, uint64_t input_data_length, huffman_tree_node** root, uint64_t* output_data_size, huffman_tree_memory_allocation_struct** huffman_tree_memory_allocation_struct, uint8_t* filling_bits_count);
+uint8_t* huffman_decode_data(uint8_t* input_data, uint64_t input_data_length, huffman_tree_node* huffman_tree, uint64_t output_data_size);
